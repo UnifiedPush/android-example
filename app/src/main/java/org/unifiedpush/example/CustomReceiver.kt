65437afe1fab5159fa7d510b8874b11927ef22d2
sim <git@sgougeon.fr>
2022-01-13 23:38:41 +0100

    Use the new Receiver

--- app/src/main/java/org/unifiedpush/example/CustomReceiver.kt
+++ app/src/main/java/org/unifiedpush/example/CustomReceiver.kt
@@ -4,10 +4,9 @@ import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 import org.unifiedpush.android.connector.MessagingReceiver
-import org.unifiedpush.android.connector.MessagingReceiverHandler
 import java.net.URLDecoder
 
-val handler = object: MessagingReceiverHandler{
+class CustomReceiver : MessagingReceiver() {
     override fun onMessage(context: Context?, message: String, instance: String) {
         val dict = URLDecoder.decode(message,"UTF-8").split("&")
         val params= dict.associate { try{it.split("=")[0] to it.split("=")[1]}catch (e: Exception){"" to ""} }
@@ -30,10 +29,6 @@ val handler = object: MessagingReceiverHandler{
         Toast.makeText(context, "Registration Failed", Toast.LENGTH_SHORT).show()
     }
 
-    override fun onRegistrationRefused(context: Context?, instance: String) {
-        Toast.makeText(context, "Registration is Refused", Toast.LENGTH_SHORT).show()
-    }
-
     override fun onUnregistered(context: Context?, instance: String){
         val broadcastIntent = Intent()
         broadcastIntent.`package` = context!!.packageName
@@ -45,5 +40,3 @@ val handler = object: MessagingReceiverHandler{
         Toast.makeText(context, "$appName is unregistered", Toast.LENGTH_SHORT).show()
     }
 }
-
-class CustomReceiver: MessagingReceiver(handler)
