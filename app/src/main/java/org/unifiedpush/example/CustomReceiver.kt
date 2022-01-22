0349533dc4ac129d683d82d292efce94d3005d23
Sparchatus <sparchatus@sparchatus.ch>
2022-01-22 18:08:30 +0100

    Use the android Work version of the connector

--- app/src/main/java/org/unifiedpush/example/CustomReceiver.kt
+++ app/src/main/java/org/unifiedpush/example/CustomReceiver.kt
@@ -2,19 +2,29 @@ package org.unifiedpush.example
 
 import android.content.Context
 import android.content.Intent
+import android.os.Looper
 import android.widget.Toast
 import org.unifiedpush.android.connector.MessagingReceiver
 import org.unifiedpush.android.connector.MessagingReceiverHandler
 import java.net.URLDecoder
 
-val handler = object: MessagingReceiverHandler{
+class CustomMessagingHandler(
+    context: Context,
+    workerParams: androidx.work.WorkerParameters
+) : MessagingReceiverHandler(context, workerParams) {
     override fun onMessage(context: Context?, message: String, instance: String) {
-        val dict = URLDecoder.decode(message,"UTF-8").split("&")
-        val params= dict.associate { try{it.split("=")[0] to it.split("=")[1]}catch (e: Exception){"" to ""} }
-        val text = params["message"]?: "New notification"
-        val priority = params["priority"]?.toInt()?: 8
-        val title = params["title"]?: context!!.getString(R.string.app_name)
-        Notifier(context!!).sendNotification(title,text,priority)
+        val dict = URLDecoder.decode(message, "UTF-8").split("&")
+        val params = dict.associate {
+            try {
+                it.split("=")[0] to it.split("=")[1]
+            } catch (e: Exception) {
+                "" to ""
+            }
+        }
+        val text = params["message"] ?: "New notification"
+        val priority = params["priority"]?.toInt() ?: 8
+        val title = params["title"] ?: context!!.getString(R.string.app_name)
+        Notifier(context!!).sendNotification(title, text, priority)
     }
 
     override fun onNewEndpoint(context: Context?, endpoint: String, instance: String) {
@@ -27,14 +37,16 @@ val handler = object: MessagingReceiverHandler{
     }
 
     override fun onRegistrationFailed(context: Context?, instance: String) {
+        if(Looper.myLooper() == null) Looper.prepare()
         Toast.makeText(context, "Registration Failed", Toast.LENGTH_SHORT).show()
     }
 
     override fun onRegistrationRefused(context: Context?, instance: String) {
+        if(Looper.myLooper() == null) Looper.prepare()
         Toast.makeText(context, "Registration is Refused", Toast.LENGTH_SHORT).show()
     }
 
-    override fun onUnregistered(context: Context?, instance: String){
+    override fun onUnregistered(context: Context?, instance: String) {
         val broadcastIntent = Intent()
         broadcastIntent.`package` = context!!.packageName
         broadcastIntent.action = UPDATE
@@ -42,8 +54,9 @@ val handler = object: MessagingReceiverHandler{
         broadcastIntent.putExtra("registered", "false")
         context.sendBroadcast(broadcastIntent)
         val appName = context.getString(R.string.app_name)
+        if(Looper.myLooper() == null) Looper.prepare()
         Toast.makeText(context, "$appName is unregistered", Toast.LENGTH_SHORT).show()
     }
 }
 
-class CustomReceiver: MessagingReceiver(handler)
+class CustomReceiver : MessagingReceiver(CustomMessagingHandler::class.java)
