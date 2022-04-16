f26ba67e90db7992794142e059751285a370ac93
sim <git@sgougeon.fr>
2022-04-16 15:01:07 +0200

    Add googleProjectNumber for FOSS embedded distrib

--- app/src/fcm/java/org/unifiedpush/example/EmbeddedDistributor.kt
+++ app/src/fcm/java/org/unifiedpush/example/EmbeddedDistributor.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import org.unifiedpush.android.embedded_fcm_distributor.EmbeddedDistributorReceiver
 
 class EmbeddedDistributor: EmbeddedDistributorReceiver() {
+
+    //toDevFossFcm//override val googleProjectNumber = "64518491375"
+
     override fun getEndpoint(context: Context, fcmToken: String, instance: String): String {
         return "https://fcm.example.unifiedpush.org/FCM?v2&instance=$instance&token=$fcmToken"
     }
