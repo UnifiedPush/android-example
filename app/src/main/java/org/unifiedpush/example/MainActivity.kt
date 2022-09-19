fa444c7b4023b96372ce9e6fc4c0c530ce5985b5
sim <git@sgougeon.fr>
2022-09-19 20:15:46 +0200

    Bump dependencies and target SDK 33

--- app/src/main/java/org/unifiedpush/example/MainActivity.kt
+++ app/src/main/java/org/unifiedpush/example/MainActivity.kt
@@ -1,16 +1,26 @@
 package org.unifiedpush.example
 
+import android.Manifest
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import org.unifiedpush.example.R
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) {/*granted ->*/
+            } .launch(
+                Manifest.permission.POST_NOTIFICATIONS
+            )
+        }
     }
 
     fun goToCheckList(view: View){
