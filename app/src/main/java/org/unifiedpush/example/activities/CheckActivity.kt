16e37e0f4dbfedcbbf6890f684e65131db090c53
sim <git@sgougeon.fr>
2023-02-12 20:25:15 +0100

    Move request to the endpoint to a new Class

--- app/src/main/java/org/unifiedpush/example/activities/CheckActivity.kt
+++ app/src/main/java/org/unifiedpush/example/activities/CheckActivity.kt
@@ -5,19 +5,14 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import com.android.volley.RequestQueue
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import org.unifiedpush.android.connector.UnifiedPush
 import org.unifiedpush.example.R
+import org.unifiedpush.example.Requests.sendNotification
 import org.unifiedpush.example.Store
 import org.unifiedpush.example.activities.MainActivity.Companion.goToMainActivity
-import org.unifiedpush.example.utils.TAG
 import org.unifiedpush.example.utils.registerOnRegistrationUpdate
 import org.unifiedpush.example.utils.updateRegistrationInfo
 
@@ -37,7 +32,11 @@ class CheckActivity : Activity() {
         }
         setEndpointOrGoToMain()
         findViewById<Button>(R.id.button_unregister).setOnClickListener { unregister() }
-        findViewById<Button>(R.id.button_notify).setOnClickListener { sendNotification() }
+        findViewById<Button>(R.id.button_notify).setOnClickListener {
+            sendNotification(this) { error ->
+                findViewById<TextView>(R.id.error_text).text = error ?: ""
+            }
+        }
         findViewById<Button>(R.id.button_reregister).setOnClickListener { reRegister() }
     }
 
@@ -79,34 +78,6 @@ class CheckActivity : Activity() {
             UnifiedPush.registerAppWithDialog(this)
         }
         Toast.makeText(applicationContext, "Registration sent.", Toast.LENGTH_SHORT).show()
-    }
-
-    private fun sendNotification() {
-        val requestQueue: RequestQueue = Volley.newRequestQueue(this)
-        val url = store.endpoint
-        val stringRequest: StringRequest =
-            object :
-                StringRequest(
-                    Method.POST, url,
-                    Response.Listener {
-                        Toast.makeText(applicationContext, "Done", Toast.LENGTH_SHORT).show()
-                        findViewById<TextView>(R.id.error_text).text = ""
-                    },
-                    Response.ErrorListener { e ->
-                        Toast.makeText(applicationContext, "An error occurred.", Toast.LENGTH_SHORT).show()
-                        Log.e(TAG, "An error occurred while testing the endpoint:\n$e")
-                        findViewById<TextView>(R.id.error_text).text = "Error:\n$e"
-                    }
-                ) {
-                override fun getParams(): MutableMap<String, String> {
-                    val params = mutableMapOf<String, String>()
-                    params["title"] = "Test"
-                    params["message"] = "With UnifiedPush"
-                    params["priority"] = "5"
-                    return params
-                }
-            }
-        requestQueue.add(stringRequest)
     }
 
     companion object {
