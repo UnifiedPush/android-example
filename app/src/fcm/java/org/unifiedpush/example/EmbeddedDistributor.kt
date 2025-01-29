2a9d62777406639f086f3e953227694d181d0ed1
sim <git@sgougeon.fr>
2025-01-29 17:03:15 +0100

    Use new EFCMD

--- app/src/fcm/java/org/unifiedpush/example/EmbeddedDistributor.kt
+++ app/src/fcm/java/org/unifiedpush/example/EmbeddedDistributor.kt
@@ -1,13 +1,8 @@
 package org.unifiedpush.example
 
-import android.content.Context
+import org.unifiedpush.android.embedded_fcm_distributor.DefaultGateway
 import org.unifiedpush.android.embedded_fcm_distributor.EmbeddedDistributorReceiver
 
 class EmbeddedDistributor : EmbeddedDistributorReceiver() {
-    @Suppress("ktlint:standard:comment-spacing")
-    //toDevFossFcm//override val googleProjectNumber = "64518491375"
-
-    override fun getEndpoint(context: Context, fcmToken: String, instance: String): String {
-        return "https://fcm.example.unifiedpush.org/FCM?v2&instance=$instance&token=$fcmToken"
-    }
+    override val gateway = DefaultGateway
 }
